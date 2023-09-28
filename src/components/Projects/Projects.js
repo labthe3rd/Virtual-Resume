9e4136cc4ef50b9912a5bf38e28cb1d4f99a02e2
Louis Bersine <louis.bersine@thepartyzonevr.com>
2023-09-28 16:18:54 -0400

    Updated MetaPlazaVR Entry
    MetaPlazaVR Update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,15 +6,15 @@ export default function Projects() {
         <p className="text-left mt-10 sm:mt-4">
           <ul className="list-disc list-inside space-y-4 text-small text-left pl-4">
             <li>
-              Rebo Tesla Cargo Lamp Rotary Dial Electrical Design – 2023 –
+              Automotive Cargo Lamp Rotary Dial Electrical Design – 2023 –
               Controls Engineer
             </li>
             <li>
-              Rebo Tesla Map Lamp/Lift Gate Automated Assembly Line Electrical
+              Automotive Map Lamp/Lift Gate Automated Assembly Line Electrical
               Design – 2023 – Controls Engineer
             </li>
             <li>
-              Hilco Stryker Gel Support Modling Press Electrical Design – 2023 –
+              Medical Gel Support Modling Press Electrical Design – 2023 –
               Controls Engineer
             </li>
             <li>
@@ -34,97 +34,93 @@ export default function Projects() {
               Controls Engineer
             </li>
             <li>
-              Cascade Engineering Harley Fairing Assembly Stations Software –
+              Motorcycle Engineering Harley Fairing Assembly Stations Software –
               2023 – Controls Engineer
             </li>
             <li>
-              Cascade Engineering Harley Air Tank Assembly/Quality Stations
+              Motorcycle Engineering Harley Air Tank Assembly/Quality Stations
               Software – 2023 – Controls Engineer
             </li>
             <li>Proficient Cloud Support System – 2022 – Controls Engineer</li>
             <li>
-              Yanfeng Mat Label Polyfuse Infusion Station Software – 2022 –
+              Automotive Mat Label Polyfuse Infusion Station Software – 2022 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Rivian Glovebox Assembly Station Software – 2022 –
+              Automotive Rivian Glovebox Assembly Station Software – 2022 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Multi Tool Camera Check Station Software – 2022 – Controls
-              Engineer
-            </li>
-            <li>
-              Royal Bracket Assembly And Packaging Rotary Dial Software – 2022 –
+              Automotive Multi Tool Camera Check Station Software – 2022 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Tesla Robot Grease Station Software – 2021 – Controls
+              Office Chair Bracket Assembly And Packaging Rotary Dial Software –
+              2022 – Controls Engineer
+            </li>
+            <li>
+              Automotive Tesla Robot Grease Station Software – 2021 – Controls
               Engineer
             </li>
             <li>
               Royal Armrest Assembly Station Software – 2021 – Controls Engineer
             </li>
             <li>
-              Yanfeng Tesla Touch Panel Assembly/Test Stations Software – 2021 –
+              Automotive Tesla Touch Panel Assembly/Test Stations Software –
+              2021 – Controls Engineer
+            </li>
+            <li>
+              Automotive Armrest Assembly Stations Software – 2021 – Controls
+              Engineer
+            </li>
+            <li>
+              Medical Footboard Assembly Software – 2021 – Controls Engineer
+            </li>
+            <li>
+              Cognex Vidi AI Assembly Line Demo Software – 2020 – Controls
+              Engineer
+            </li>
+            <li>
+              Automotive Interior Panel Assembly Press Software – 2020 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Armrest Assembly Stations Software – 2021 – Controls
-              Engineer
-            </li>
-            <li>
-              Hilco Stryker Footboard Assembly Software – 2021 – Controls
-              Engineer
-            </li>
-            <li>
-              Perrigo Cognex Vidi AI Assembly Line Demo Software – 2020 –
+              Automotive Interior Panel Sonic Welding Press Software – 2020 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Interior Panel Assembly Press Software – 2020 – Controls
+              Automotive Door Panel Assembly Stations Software – 2020 – Controls
               Engineer
             </li>
             <li>
-              Yanfeng Interior Panel Sonic Welding Press Software – 2020 –
+              Automotive Fully Automated Latch Assembly Rotary Dial Software –
+              2020 – Controls Engineer
+            </li>
+            <li>
+              AutoCAD Electrical Network Server – 2019 – Controls Engineer
+            </li>
+            <li>Automated Coil Assembly Station – 2019 – Controls Engineer</li>
+            <li>
+              Metal Refinery Process Equipment Software Upgrade – 2019 –
               Controls Engineer
             </li>
             <li>
-              Yanfeng Door Panel Assembly Stations Software – 2020 – Controls
+              Welding System Assembly Management – 2018 – Controls Engineer
+            </li>
+            <li>
+              Automotive Hood Latch Assembly Station Software – 2018 – Controls
               Engineer
             </li>
+            <li>Molding Station Software – 2018 – Controls Engineer</li>
             <li>
-              Ess Tec Fully Automated Latch Assembly Rotary Dial Software – 2020
-              – Controls Engineer
+              Semi Truck Interior Molding Software – 2018 – Controls Engineer
             </li>
             <li>
-              Proficient AutoCAD Electrical Network Server – 2019 – Controls
-              Engineer
-            </li>
-            <li>
-              Roman Automated Coil Assembly Station – 2019 – Controls Engineer
-            </li>
-            <li>
-              Roman Metal Refinery Process Equipment Software Upgrade – 2019 –
+              Automotive Seat Assembly Line Pushnut Robot Upgrade – 2017 –
               Controls Engineer
             </li>
             <li>
-              Miller Jetline Assembly Management – 2018 – Controls Engineer
-            </li>
-            <li>
-              Adient Hood Latch Assembly Station Software – 2018 – Controls
-              Engineer
-            </li>
-            <li>Paragon Molding Station Software – 2018 – Controls Engineer</li>
-            <li>
-              Hoekstra Semi Interior Molding Software – 2018 – Controls Engineer
-            </li>
-            <li>
-              Adient Seat Assembly Line Pushnut Robot Upgrade – 2017 – Controls
-              Engineer
-            </li>
-            <li>
-              Adient Seat Assembly Line Software – 2017 – Controls Engineer
+              Automotive Seat Assembly Line Software – 2017 – Controls Engineer
             </li>
             <li>
               MRC4 Rollstand Software Development – 2016 – Associates Controls
--- src/components/Showcase/MetaPlazaVR.js
+++ src/components/Showcase/MetaPlazaVR.js
@@ -5,29 +5,13 @@ export default function MetaPlazaVR() {
       <h2 className="font-semibold text-xl">Lead Developer/Owner</h2>
       <h3 className="italic">June 2023 - Current</h3>
       <p className="mt-10 sm:mt-4">
-        Since stepping into my role as Lead Developer and Co-Owner at Metaplaza
-        VR in June 2023, I've been riding the crest of a wave where innovation
-        meets community engagement. Our aim was audacious yet grounded: to
-        empower the VRChat community with a revenue-generating tool,
-        transforming virtual worlds into economic landscapes. And how did we
-        plan to do this? By creating a Unity prefab that seamlessly integrates
-        community-based advertisements into these immersive spaces. My
-        responsibilities were as multi-dimensional as the worlds we were
-        enhancing. I set up a robust server architecture using NGINX, ensuring a
-        reliable backbone for our digital endeavors. On the software side, I
-        crafted a dynamic backend using Node.js, which served as the engine room
-        for our service. But what's an engine without a dashboard? I designed a
-        compelling frontend using nextJS, React, and JavaScript, delivering an
-        intuitive user experience that belies the complexity of the backend. As
-        for the Unity prefab—where our idea comes to life in the VR world—I
-        masterminded its development using C#, encapsulating our vision into a
-        tangible asset. But we didn't stop at implementation; our plan also
-        includes a data-driven approach to measure impact. We aim to collect
-        rich metrics to understand from which platform and world location views
-        are originating, thereby fine-tuning our offerings based on actual user
-        engagement. At Metaplaza VR, I am not just a developer or a business
-        owner; I am a catalyst for community empowerment and a pioneer at the
-        intersection of virtual reality and real-world opportunity.
+        <span className="font-bold text-danger-400">
+          This service is not active to be respectful to VRChat's terms of
+          service, but I've included it as a means to share my skills/abilities.
+        </span>
+        The video shows a project I worked on that used VRChat's Udon Sharp,
+        which is essentially C# in unity, to integrate with a backend server I
+        created. I also created the font end. Communicaton was handled via APIs.
       </p>
       <iframe
         className="mt-4 md:mt-10 w-full aspect-video"
