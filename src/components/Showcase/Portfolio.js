de2088502d573cfe9d412e402cee1bd0ef2ba572
Louis Bersine <labthe3rd@gmail.com>
2023-11-02 15:27:36 -0400

    Removed MetaPlazaVR from experience
    Removed MetaPlazaVR from experience since the project did not work out.

--- src/components/Showcase/Portfolio.js
+++ src/components/Showcase/Portfolio.js
@@ -9,7 +9,6 @@ export default function Portfolio() {
   return (
     <div className="grid justify-items-center mt-8 mb-8 gap-4">
       <div className="text-2xl sm:text-5xl">Video Portfolio</div>
-      <MetaPlazaVR />
       <Universum />
       <Delirium />
       <ThePartyZone />
